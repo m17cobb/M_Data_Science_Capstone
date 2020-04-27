655ab278c67e5f5ad4e313694448d66573c917b0
m17cobb <64418293+m17cobb@users.noreply.github.com>
2020-04-27 11:35:31 -0400

    Add files via upload

---
+++ Week_1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ***Data Science Capstone Project***
+# 
+# *This notebook will mainly be used for my capstone project*
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[6]:
+
+
+print("Hello Capstone Project Course!")
+
